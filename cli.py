1e70cf3106909b0522cb5a700adbb805811db6c2
las02 <lasse101010@gmail.com>
2024-11-08 09:11:32 +0100

    .

--- cli.py
+++ cli.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-try: 
-  import rich_click as click
+try:
+    import rich_click as click
 except ModuleNotFoundError as e:
-  print("""\nCould not find rich_click, please make sure to create an environment containing 
+    print("""\nCould not find rich_click, please make sure to create an environment containing 
 the dependency eg. using conda or pip. See the user guide on the github README\n""")
-  raise e
+    raise e
 
 
 import os
@@ -19,51 +19,54 @@ class Logger:
     def print(self, arg):
         click.echo(arg)
 
-            
 
 # Snakemake should handle genomad download
 # Only downloading conda envs
-#snakemake --use-conda --conda-create-envs-only -c4 --conda-frontend conda
+# snakemake --use-conda --conda-create-envs-only -c4 --conda-frontend conda
 # genomad database download?
-        
-#def validate_paths(self):
 
-class Snakemake_runner():
+# def validate_paths(self):
+
+
+class Snakemake_runner:
     argument_holder = []
+
     def __init__(self):
         dir_of_current_file = os.path.dirname(os.path.realpath(__file__))
-        self.snakemake_path = shutil.which("snakemake")  
+        self.snakemake_path = shutil.which("snakemake")
         self.snakefile_path = Path(Path(dir_of_current_file) / "snakefile")
         self.add_argument("--snakefile", self.snakefile_path)
         self.add_argument("-c", "4")
 
     def add_argument(self, argument, command):
-      self.argument_holder += [argument, command]
+        self.argument_holder += [argument, command]
 
     def run_snakemake(self):
-      print([self.snakemake_path] + self.argument_holder)
-      #subprocess.run([self.snakemake_path, "--snakefile" ,self.snakefile, "-c", self.threads])
-    
+        cmd = [self.snakemake_path] + self.argument_holder
+        print(cmd)
+        # subprocess.run(cmd)
+
     def validate_paths(self):
-      if not self.snakefile_path.exists():
-        raise click.UsageError(f"Could not find snakefile, tried: {self.snakefile_path}")
+        if not self.snakefile_path.exists():
+            raise click.UsageError(
+                f"Could not find snakefile, tried: {self.snakefile_path}"
+            )
 
-      if self.snakemake_path is None:
-        raise click.UsageError(f"Could not find snakemake, is it installed? \nsee https://snakemake.readthedocs.io/en/stable/getting_started/installation.html")
+        if self.snakemake_path is None:
+            raise click.UsageError(
+                f"Could not find snakemake, is it installed? \nsee https://snakemake.readthedocs.io/en/stable/getting_started/installation.html"
+            )
 
-      
 
 @click.command()
-#@click.option("--genomad_db", help="genomad database", type=click.Path(exists=True))
+# @click.option("--genomad_db", help="genomad database", type=click.Path(exists=True))
 @click.option("--dryrun", help="run a dryrun", is_flag=True)
 def main(dryrun):
-    
-  # if genomad_db == None:
-  # raise click.BadParameter("genomad_db", param_hint=["--genomad_db"])
+    # if genomad_db == None:
+    # raise click.BadParameter("genomad_db", param_hint=["--genomad_db"])
 
     logger = Logger()
     logger.print("running snakemake")
-
 
     runner = Snakemake_runner()
     runner.validate_paths()
@@ -71,7 +74,6 @@ def main(dryrun):
 
 
 if __name__ == "__main__":
-
     main()
 
     #    status = snakemake.snakemake(snakefile, configfile=paramsfile,
